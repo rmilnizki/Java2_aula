dfe071364694a1eabccd54ac13ad43e59835f7d3
rmilnizki <37422717+rmilnizki@users.noreply.github.com>
2018-03-15 20:07:23 -0300

    Update conexaoJDBC.java

--- java2_aula_jdbc/src/github/conexaoJDBC.java
+++ java2_aula_jdbc/src/github/conexaoJDBC.java
@@ -3,6 +3,6 @@ package github;
 
 public class conexaoJDBC {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("Teste por GitHub");
     }
 }
